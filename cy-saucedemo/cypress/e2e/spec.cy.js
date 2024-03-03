bb1f16ba18557921545bc7c343f81873065dff4b
Travis <travis.anderson30@gmail.com>
2024-03-03 10:28:39 -0700

    Add cypress to project for saucedemo testing

---
+++ cy-saucedemo/cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('Saucedemo Happy Path', () => {
+  it('Logs in to the Saucedemo website', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+
+    cy.url().should('include', '/inventory')
+  })
+})
