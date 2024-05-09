93c78fc456cb690b215b146de7cb4f6a6e60af36
Travis <travis.anderson30@gmail.com>
2024-05-09 10:01:10 -0600

    Remove saucedemo tests for now (there was nothing in there)

--- saucedemo/test/test_signin.py
+++
@@ -1,7 +0,0 @@
-from src.pages.signin_page import SignInpage
-
-#Add test_ prefix to run test again
-def standard_user_can_sign_in(browser):
-    sign_in_page = SignInpage(browser)
-
-    sign_in_page.login('standard_user', 'secret_sauce')
