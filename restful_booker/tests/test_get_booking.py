06a3ec57a50a6dd20a5f43b1ab0d28c224dd8ce3
Travis <travis.anderson30@gmail.com>
2024-05-15 21:04:58 -0600

    Missed changes to one file

--- restful_booker/tests/test_get_booking.py
+++ restful_booker/tests/test_get_booking.py
@@ -1,4 +1,3 @@
-import json
 from assertpy import assert_that
 import requests
 
@@ -13,87 +12,116 @@ def test_can_get_all_booking_ids():
 
     assert_that(response.status_code).is_equal_to(200)
 
-
-def test_can_create_new_booking(create_new_booking):
+def test_can_get_booking_id_by_full_name(create_new_booking):
     """
-    Test the creating a new booking with a valid request body creates the new
-    booking and returns information about the booing
+    Test that requesting a booking id filtering by first and last name
+    as query params returns just that booking id
     """
-    assert_that(create_new_booking.status_code).is_equal_to(200)
+    new_booking_json = create_new_booking.json()
+    booking_id = new_booking_json['bookingid']
+    fname = new_booking_json['booking']['firstname']
+    lname = new_booking_json['booking']['lastname']
+    query = f'?firstname={fname}&lastname={lname}'
+    response = requests.get(f'{BASE_URI}/booking{query}', timeout=10000)
 
-
-def test_can_update_booking(get_token, create_new_booking):
-    """
-    Can update an existing booking with valid request body
-    """
-    _, token = get_token
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Cookie': f'token={token}'
-    }
-
-    # Get response and update the 'depositpaid' field to False
-    new_booking = create_new_booking.json()
-    booking_id = new_booking['bookingid']
-    updated_booking = new_booking['booking']
-    updated_booking['depositpaid'] = False
-
-    # Send PUT request to update booking info
-    payload = json.dumps(updated_booking)
-    response = requests.put(f'{BASE_URI}/booking/{booking_id}', data=payload, headers=headers, timeout=10000)
+    id_list = [id['bookingid'] for id in response.json()]
 
     assert_that(response.status_code).is_equal_to(200)
-    assert_that(response.json()['depositpaid']).is_false()
+    assert_that(id_list).contains(booking_id)
 
 
-def test_can_update_booking_with_partial_payload(get_token, create_new_booking):
+def test_can_get_booking_id_by_first_name(create_new_booking):
     """
-    Can update existing booking by sending a partial payload
+    Test that requesting a booking id filtering by first and last name
+    as query params returns just that booking id
     """
-    _, token = get_token
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Cookie': f'token={token}'
-    }
+    new_booking_json = create_new_booking.json()
+    booking_id = new_booking_json['bookingid']
+    fname = new_booking_json['booking']['firstname']
+    query = f'?firstname={fname}'
+    response = requests.get(f'{BASE_URI}/booking{query}', timeout=10000)
 
-    # Create new booking and retrieve id
-    new_booking = create_new_booking.json()
-    booking_id = new_booking['bookingid']
-
-    # Send partial payload to update first and last name
-    payload = json.dumps({
-        'firstname': 'Ethan',
-        'lastname': 'Stanfield'
-    })
-    response = requests.patch(f'{BASE_URI}/booking/{booking_id}', data=payload, headers=headers, timeout=10000)
+    id_list = [id['bookingid'] for id in response.json()]
 
     assert_that(response.status_code).is_equal_to(200)
-    assert_that(response.json()['firstname']).is_equal_to('Ethan')
-    assert_that(response.json()['lastname']).is_equal_to('Stanfield')
+    assert_that(id_list).contains(booking_id)
 
 
-def test_can_delete_booking(get_token, create_new_booking):
+def test_can_get_booking_id_by_last_name(create_new_booking):
     """
-    Can delete a booking by providing the booking ID
+    Test that requesting a booking id filtering by first and last name
+    as query params returns just that booking id
     """
-    _, token = get_token
-    headers = {
-        'Content-Type': 'application/json',
-        'Cookie': f'token={token}'
-    }
+    new_booking_json = create_new_booking.json()
+    booking_id = new_booking_json['bookingid']
+    lname = new_booking_json['booking']['lastname']
+    query = f'?&lastname={lname}'
+    response = requests.get(f'{BASE_URI}/booking{query}', timeout=10000)
 
-    # Create new booking and retrieve id
-    new_booking = create_new_booking.json()
-    booking_id = new_booking['bookingid']
+    id_list = [id['bookingid'] for id in response.json()]
 
-    # Delete booking and verify a 201 status which indicates successful deletion in this API
-    response = requests.delete(f'{BASE_URI}/booking/{booking_id}', headers=headers, timeout=10000)
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(id_list).contains(booking_id)
 
-    assert_that(response.status_code).is_equal_to(201)
-    
-    # Verify the deleted booking cannot be found
-    deleted_booking = requests.get(f'{BASE_URI}/booking/{booking_id}', timeout=10000)
 
-    assert_that(deleted_booking.status_code).is_equal_to(404)
+def test_can_get_booking_id_by_checkin_date(create_new_booking):
+    """
+    According to documentation, passing query param of checkin date retrieves
+    booking ids with date greater than or equal to date.
+    Retrieving booking id with checkin date after query param date returns
+    the expected booking id
+    """
+    new_booking_json = create_new_booking.json()
+    booking_id = new_booking_json['bookingid']
+    query = '?checkin=2023-07-20'
+
+    # In the absence of a datetime object as the checkin/checkout dates, I am hardcoding this
+    # value as I won't be dynamically generating it nor can I modify it
+    # See known issues as to why I can't pass the checkin date from the response itself
+    response = requests.get(f'{BASE_URI}/booking{query}', timeout=10000)
+    id_list = [id['bookingid'] for id in response.json()]
+
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(id_list).contains(booking_id)
+
+
+def test_can_get_booking_id_by_checkout_date(create_new_booking):
+    """
+    According to documentation, passing query param of checkin date retrieves
+    booking ids with date greater than or equal to date.
+    Retrieving booking id with checkin date after query param date returns
+    the expected booking id
+    """
+    new_booking_json = create_new_booking.json()
+    booking_id = new_booking_json['bookingid']
+    query = '?checkout=2023-07-23'
+
+    # In the absence of a datetime object as the checkin/checkout dates, I am hardcoding this
+    # value as I won't be dynamically generating it nor can I modify it
+    # See known issues as to why I can't pass the checkin date from the response itself
+    response = requests.get(f'{BASE_URI}/booking{query}', timeout=10000)
+    id_list = [id['bookingid'] for id in response.json()]
+
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(id_list).contains(booking_id)
+
+
+def test_can_get_booking_id_by_checkin_and_checkout_date(create_new_booking):
+    """
+    According to documentation, passing query param of checkin date retrieves
+    booking ids with date greater than or equal to date.
+    Retrieving booking id with checkin date after query param date returns
+    the expected booking id
+    """
+    new_booking_json = create_new_booking.json()
+    booking_id = new_booking_json['bookingid']
+    query = '?checkin=2023-07-20&checkout=2023-07-23'
+
+    # In the absence of a datetime object as the checkin/checkout dates, I am hardcoding this
+    # value as I won't be dynamically generating it nor can I modify it
+    # See known issues as to why I can't pass the checkin date from the response itself
+    response = requests.get(f'{BASE_URI}/booking{query}', timeout=10000)
+    id_list = [id['bookingid'] for id in response.json()]
+
+    assert_that(response.status_code).is_equal_to(200)
+    assert_that(id_list).contains(booking_id)
