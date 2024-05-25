d2250e44931ddb2c20d1557e95e974ee8d7258cc
Travis <travis.anderson30@gmail.com>
2024-05-25 12:32:18 -0600

    Set up selenium to run on parabank website using page object model

---
+++ py-parabank/locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+class LoginPageLocators(object):
+    USERNAME = (By.NAME, "username")
+    PASSWORD = (By.NAME, "password")
+    LOGIN_BUTTON = (By.XPATH, "//input[@value='Log In']")
+
+class LoginResultPageLocators(object):
+    ERROR_MESSAGE = (By.CLASS_NAME, "error")
